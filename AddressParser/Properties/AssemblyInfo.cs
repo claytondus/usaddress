2c796d552e22d06b8d858c1e6d7bffafaf046524
Clayton Davis <cd@ae4ax.net>
2016-11-20 10:49:21 -0500

    Update to VS2017 RC project format

--- AddressParser/Properties/AssemblyInfo.cs
+++ AddressParser/Properties/AssemblyInfo.cs
@@ -10,13 +10,13 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("AddressParser")]
+[assembly: AssemblyTitle("Claytondus.AddressParser")]
 [assembly: AssemblyDescription("This is a partial port of the Perl Geo::StreetAddress::US CPAN module to C#.")]
-[assembly: AssemblyCompany("Jim Counts")]
-[assembly: AssemblyProduct("AddressParser")]
-[assembly: AssemblyCopyright("Copyright © Jim Counts 2011")]
+[assembly: AssemblyCompany("Jim Counts, Clayton Davis")]
+[assembly: AssemblyProduct("Claytondus.AddressParser")]
+[assembly: AssemblyCopyright("Copyright © Jim Counts, Clayton Davis 2016")]
 [assembly: ComVisible(false)]
 [assembly: Guid("364a8dcc-05a8-4b05-a8e7-9622fdf6f562")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("0.0.19")]
+[assembly: AssemblyFileVersion("0.1.0")]
 [assembly: CLSCompliant(true)]
